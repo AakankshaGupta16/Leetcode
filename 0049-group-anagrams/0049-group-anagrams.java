d72623275730be135ac2cefdee0ece9f52a454a4
Aakanksha <138550872+AakankshaGupta16@users.noreply.github.com>
2025-10-22 10:30:39 +0530

    Time: 7 ms (62.68%), Space: 47.8 MB (83.99%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) 
+    {
+        HashMap<String,List<String>> mpp=new HashMap<>();
+        for(String s:strs)
+        {
+            char ch[]=s.toCharArray();
+            Arrays.sort(ch);
+            String key=new String(ch);
+            if(!mpp.containsKey(key))
+            mpp.put(key,new ArrayList<>());
+            mpp.get(key).add(s);
+        }
+        return new ArrayList<>(mpp.values());
+        
+    }
+}
