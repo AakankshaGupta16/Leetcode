9842f126e8a491cee98824b94e20cbc09b266b55
Aakanksha <138550872+AakankshaGupta16@users.noreply.github.com>
2025-10-28 12:03:20 +0530

    Time: 4 ms (76.94%), Space: 45 MB (11.14%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,18 +3,16 @@ class Solution {
     {
         int sl=s.length();
         int tl=t.length();
-        if(sl!=tl)
-        return false;
+        if(sl!=tl) return false;
         char ch1[]=s.toCharArray();
         char ch2[]=t.toCharArray();
         Arrays.sort(ch1);
         Arrays.sort(ch2);
-        for(int i=0;i<ch1.length;i++)
+        for(int i=0;i<sl;i++)
         {
             if(ch1[i]!=ch2[i])
             return false;
         }
         return true;
-        
     }
 }
