4b7460834b4fd63cd2739d9e1e820f319a87bb34
Aakanksha <138550872+AakankshaGupta16@users.noreply.github.com>
2025-11-01 23:57:19 +0530

    Time: 17 ms (13.31%), Space: 47.6 MB (81.2%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,17 +1,14 @@
 class Solution {
-    public int[] topKFrequent(int[] nums, int k) {
-        // Step 1: Count frequencies
-        Map<Integer, Integer> count = new HashMap<>();
-        for (int n : nums)
-         count.put(n, count.getOrDefault(n, 0) + 1);    
-        // Step 2: Create a list of unique numbers
-        List<Integer> list = new ArrayList<>(count.keySet());
-        // Step 3: Sort the list by frequency in descending order
-        list.sort((a, b) -> count.get(b) - count.get(a)); // highest frequency first
-        // Step 4: Take first k elements
-        int[] ans = new int[k];
-        for (int i = 0; i < k; i++) 
-        ans[i] = list.get(i);
+    public int[] topKFrequent(int[] nums, int k) 
+    {
+        Map<Integer,Integer> count=new HashMap<>();
+        for(int n:nums)
+        count.put(n,count.getOrDefault(n,0)+1);
+        List<Integer> list=new ArrayList<>(count.keySet());
+        list.sort((a,b)->count.get(b)-count.get(a));
+        int ans[]=new int[k];
+        for(int i=0;i<k;i++)
+        ans[i]=list.get(i);
         return ans;
     }
 }
