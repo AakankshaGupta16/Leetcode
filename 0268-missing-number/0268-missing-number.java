b55ff95b024f6d27247b256901bc6f06b4b28316
Aakanksha <138550872+AakankshaGupta16@users.noreply.github.com>
2026-02-10 10:40:19 +0530

    Time: 0 ms (100%), Space: 47.2 MB (91.09%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,17 +1,16 @@
 class Solution {
-    public int missingNumber(int[] nums) 
-    {
-        int n=nums.length;
-        int hash[]=new int[n+1];
-        for(int i=0;i<n;i++)
-        {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int hash[] = new int[n + 1];
+
+        for (int i = 0; i < n; i++) {
             hash[nums[i]]++;
         }
-        for(int i=0;i<=n;i++)
-        {
-            if(hash[i]==0)
-            return i;
+
+        for (int i = 0; i <= n; i++) {
+            if (hash[i] == 0) return i;
         }
-        return -1;
+
+        return -1; // never reached
     }
 }
