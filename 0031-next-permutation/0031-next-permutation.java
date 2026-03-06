5147a90bbdaca9cf714f1f64ae89f56ff19f10da
Aakanksha <138550872+AakankshaGupta16@users.noreply.github.com>
2026-03-06 17:41:39 +0530

    Time: 0 ms (100%), Space: 44.3 MB (96.61%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,38 +1,46 @@
 class Solution {
-    public void nextPermutation(int[] arr) 
-    {
-        int ind=0;
-        for(int i=n-1;i>=0;i--)
-        {
-            if(arr[i-1]<arr[i])
-            {
-                ind=i;
+    public void nextPermutation(int[] arr) {
+
+        int n = arr.length;
+        int ind = -1;
+
+        // Step 1: find breakpoint
+        for(int i = n-2; i >= 0; i--) {
+            if(arr[i] < arr[i+1]) {
+                ind = i;
                 break;
             }
         }
-        for(int i=n-1;i>=0;i--)
-        {
-            if(arr[i-1]>arr[i])
-            swap(arr,i,ind);
+
+        // Step 2: if no breakpoint
+        if(ind == -1) {
+            reverse(arr, 0, n-1);
+            return;
         }
 
-    }
-    public void swap(int arr[], int i1,int ind)
-    {
-        int t=arr[i1];
-        arr[i1]=arr[ind];
-        arr[ind]=t;
-        reverse(arr,i1);
+        // Step 3: find next greater
+        for(int i = n-1; i > ind; i--) {
+            if(arr[i] > arr[ind]) {
+                swap(arr, i, ind);
+                break;
+            }
+        }
 
+        // Step 4: reverse right part
+        reverse(arr, ind+1, n-1);
     }
-    public void reverse(int arr,int i2)
-    {
-        int n=arr.length;
-        for(int i=0;i<i2/2;i++)
-        {
-            int t=arr[i];
-            arr[i]=arr[i2];
-            arr[i2]=t;
+
+    public void swap(int[] arr, int i, int j) {
+        int t = arr[i];
+        arr[i] = arr[j];
+        arr[j] = t;
+    }
+
+    public void reverse(int[] arr, int l, int r) {
+        while(l < r) {
+            swap(arr, l, r);
+            l++;
+            r--;
         }
     }
 }
