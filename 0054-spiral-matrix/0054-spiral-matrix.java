b946f4abf68ff755d876682fc6542f2e620b399a
Aakanksha <138550872+AakankshaGupta16@users.noreply.github.com>
2026-03-09 02:45:44 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,42 +1,34 @@
-import java.util.*;
-
 class Solution {
-    public List<Integer> spiralOrder(int[][] mat) {
-        List<Integer> ans = new ArrayList<>();
-
-        int t = 0, b = mat.length - 1;
-        int l = 0, r = mat[0].length - 1;
-
-        while (t <= b && l <= r) {
-            // Traverse top row
-            for (int i = l; i <= r; i++) {
-                ans.add(mat[t][i]);
-            }
+    public List<Integer> spiralOrder(int[][] mat) 
+    {
+        List<Integer> ans=new ArrayList<>();
+        int t = 0;
+        int b = mat.length - 1;
+        int l = 0;
+        int r = mat[0].length - 1;
+        while(t<=b && l<=r)
+        {
+            for(int i=l;i<=r;i++)
+            ans.add(mat[t][i]);
             t++;
-
-            // Traverse right column
-            for (int i = t; i <= b; i++) {
-                ans.add(mat[i][r]);
-            }
+            for(int i=t;i<=b;i++)
+            ans.add(mat[i][r]);
             r--;
-
-            // Traverse bottom row (check boundary)
-            if (t <= b) {
-                for (int i = r; i >= l; i--) {
-                    ans.add(mat[b][i]);
-                }
+            if(t<=b)
+            {
+                for(int i=r;i>=l;i--)
+                ans.add(mat[b][i]);
                 b--;
             }
-
-            // Traverse left column (check boundary)
-            if (l <= r) {
-                for (int i = b; i >= t; i--) {
-                    ans.add(mat[i][l]);
-                }
+            if(l<=r)
+            {
+                for(int i=b;i>=t;i--)
+                ans.add(mat[i][l]);
                 l++;
             }
+            
         }
-
-        return ans;
+return ans;
+        
     }
 }
