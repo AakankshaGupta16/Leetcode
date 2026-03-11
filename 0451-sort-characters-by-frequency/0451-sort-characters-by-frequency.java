c23427df1c90bf407a43bc519b10c58262b720cd
Aakanksha <138550872+AakankshaGupta16@users.noreply.github.com>
2026-03-11 15:33:11 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,24 +1,38 @@
 class Solution {
     public String frequencySort(String s) 
     {
-        //Step1:count frequency
-        HashMap<Character,Integer> freq=new HashMap<>();
-        for(char c:s.toCharArray())
+        // Step 1: Count frequency
+        Map<Character, Integer> freq = new HashMap<>();
+        for(char c : s.toCharArray())
+            freq.put(c, freq.getOrDefault(c, 0) + 1);
+
+        // Step 2: Create buckets
+        List<Character>[] bucket = new List[s.length() + 1];
+
+        for(char c : freq.keySet())
         {
-            freq.put(c,freq.getOrDefault(c,0)+1);
+            int f = freq.get(c);
+            if(bucket[f] == null)
+                bucket[f] = new ArrayList<>();
+
+            bucket[f].add(c);
         }
-        //Step2: Sort character by frequency (descending)
-        List<Character> chars=new ArrayList<>(freq.keySet());
-        Collections.sort(chars,(a,b) -> freq.get(b) - freq.get(a));
-        //Step3: Build result
-        StringBuilder sb=new StringBuilder();
-        for(char c:chars)
+
+        // Step 3: Build result
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = bucket.length - 1; i >= 0; i--)
         {
-            int count=freq.get(c);
-            for(int i=0;i<count;i++)
-            sb.append(c);
+            if(bucket[i] != null)
+            {
+                for(char c : bucket[i])
+                {
+                    for(int j = 0; j < i; j++)
+                        sb.append(c);
+                }
+            }
         }
+
         return sb.toString();
     }
-    
 }
