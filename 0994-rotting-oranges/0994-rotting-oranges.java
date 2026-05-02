c32c2e08eb10b85368aff61b3742f75dd08be0e2
Aakanksha <138550872+AakankshaGupta16@users.noreply.github.com>
2026-05-02 19:04:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -0,0 +1,66 @@
+class Pair
+{
+    int r,c,t;
+    Pair(int row,int col,int tm)
+    {
+        this.r=row;
+        this.c=col;
+        this.t=tm;
+    }
+
+}
+
+class Solution 
+{
+    public int orangesRotting(int[][] grid) 
+    {
+        
+        Queue<Pair> q=new LinkedList<>();
+        int n=grid.length;
+        int m=grid[0].length;
+        int cntFresh=0;
+        int vis[][]=new int[n][m];
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(grid[i][j]==2)
+                {
+                    q.add(new Pair(i,j,0));
+                    vis[i][j]=2;
+                }
+                else
+                {
+                    vis[i][j]=0;
+                }
+                if(grid[i][j]==1) cntFresh++;
+                
+            }
+        }
+        int tm=0;
+        int cnt=0;
+        int drow[]={-1,0,+1,0};
+        int dcol[]={0,+1,0,-1};
+        while(!q.isEmpty())
+        {
+            Pair p=q.poll();
+            int row=p.r;
+            int col=p.c;
+            int t=p.t;
+            tm=Math.max(tm,t);
+            for(int i=0;i<4;i++)
+            {
+                int nrow=row+drow[i];
+                int ncol=col+dcol[i];
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]==0 && grid[nrow][ncol]==1)
+                {
+                    q.add(new Pair(nrow,ncol,t+1));
+                    vis[nrow][ncol]=2;
+                    cnt++;
+                }
+            }
+        }
+        if(cnt!=cntFresh) return -1;
+        return tm;
+    }
+}
