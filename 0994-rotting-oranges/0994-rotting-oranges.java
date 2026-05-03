99f45b639f2beef448c5a6660664e0ddcc7d03d7
Aakanksha <138550872+AakankshaGupta16@users.noreply.github.com>
2026-05-03 13:41:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,24 +1,21 @@
 class Pair
 {
     int r,c,t;
-    Pair(int row,int col,int tm)
+    Pair(int row, int col,int time)
     {
         this.r=row;
         this.c=col;
-        this.t=tm;
+        this.t=time;
     }
-
 }
-
 class Solution 
 {
     public int orangesRotting(int[][] grid) 
     {
-        
-        Queue<Pair> q=new LinkedList<>();
         int n=grid.length;
         int m=grid[0].length;
-        int cntFresh=0;
+        int freshcnt=0;
+        Queue<Pair> q=new LinkedList<>();
         int vis[][]=new int[n][m];
         for(int i=0;i<n;i++)
         {
@@ -26,15 +23,15 @@ class Solution
             {
                 if(grid[i][j]==2)
                 {
-                    q.add(new Pair(i,j,0));
                     vis[i][j]=2;
+                    q.add(new Pair(i,j,0));
                 }
-                else
+                else if(grid[i][j]==0)
                 {
                     vis[i][j]=0;
                 }
-                if(grid[i][j]==1) cntFresh++;
-                
+                else if(grid[i][j]==1)
+                freshcnt++;
             }
         }
         int tm=0;
@@ -47,7 +44,7 @@ class Solution
             int r=p.r;
             int c=p.c;
             int t=p.t;
-            tm=Math.max(tm,t);
+            tm=Math.max(t,tm);
             for(int i=0;i<4;i++)
             {
                 int nrow=r+drow[i];
@@ -55,12 +52,14 @@ class Solution
                 if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]==0 && grid[nrow][ncol]==1)
                 {
                     q.add(new Pair(nrow,ncol,t+1));
-                    vis[nrow][ncol]=2;
+                    vis[nrow][ncol]=1;
                     cnt++;
                 }
             }
         }
-        if(cnt!=cntFresh) return -1;
+        if(cnt!=freshcnt) return -1;
         return tm;
+
+        
     }
 }
