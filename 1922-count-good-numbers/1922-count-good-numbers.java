d0305fb603b2435844c9e884563bf2a776d9d725
Aakanksha <138550872+AakankshaGupta16@users.noreply.github.com>
2026-05-16 16:36:51 +0530

    Time: 0 ms (100%), Space: 42.2 MB (51.42%) - LeetHub

---
+++ 1922-count-good-numbers/1922-count-good-numbers.java
@@ -0,0 +1,27 @@
+class Solution 
+{
+    //CHOICES FOR EVEN INDEX: 0,2,4,6,8
+    //ODD: 2,3,5,7
+    //ANS: POW(5,even-pos)*POW(4,odd-pos)
+    long MOD=1000000007;
+    public int countGoodNumbers(long n) 
+    {
+        long even=(n+1)/2;
+        long odd=n/2;
+        long ans= (pow(5,even)*pow(4,odd)) % MOD;
+        return (int) ans;
+       
+    }
+    public long pow(long x,long n)
+    {
+        
+        if(n==0)
+        return 1;
+        long half=pow(x,n/2);
+        if(n%2==0)
+        return (half*half)%MOD;
+        else 
+        return (half*half)*x %MOD;
+
+    }
+}
