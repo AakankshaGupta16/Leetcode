41e9e6799e2902e07678152d00c4c0377008370a
Aakanksha <138550872+AakankshaGupta16@users.noreply.github.com>
2026-05-17 16:50:38 +0530

    Time: 3 ms (55.95%), Space: 45.3 MB (10.09%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -8,26 +8,16 @@ class Solution
         solve(nums, 0, new ArrayList<>());
         return ans;
     }
-
     public void solve(int[] nums, int start, List<Integer> list)
     {
-        // Current subset is valid
-        ans.add(new ArrayList<>(list));
-
+        ans.add(new ArrayList<>(list));// Current subset is valid
         for(int i = start; i < nums.length; i++)
-        {
-            // Skip duplicates at same recursion level
-            if(i > start && nums[i] == nums[i - 1])
+        { 
+            if(i > start && nums[i] == nums[i - 1])// Skip duplicates at same recursion level
                 continue;
-
-            // Take current element
-            list.add(nums[i]);
-
-            // Recurse for next indices
-            solve(nums, i + 1, list);
-
-            // Backtrack
-            list.remove(list.size() - 1);
+            list.add(nums[i]);// Take current element            
+            solve(nums, i + 1, list);// Recurse for next indices
+            list.remove(list.size() - 1);// Backtrack
         }
     }
 }
