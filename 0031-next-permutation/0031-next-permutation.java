922d11108f2925f15246cc1c0e2de21bb01e9df8
Aakanksha <138550872+AakankshaGupta16@users.noreply.github.com>
2026-07-02 12:09:23 +0530

    Time: 0 ms (100%), Space: 44.7 MB (50.8%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,44 +1,44 @@
 class Solution {
-    public void nextPermutation(int[] arr) {
-
-        int n = arr.length;
-        int ind = -1;
-
-        // Step 1: find breakpoint
-        for(int i = n-2; i >= 0; i--) {
-            if(arr[i] < arr[i+1]) {
-                ind = i;
+    public void nextPermutation(int[] nums) 
+    {
+        int n=nums.length;
+        int ind=-1;
+        for(int i=n-2;i>=0;i--)
+        {
+            if(nums[i]<nums[i+1])
+            {
+                ind =i;
                 break;
             }
         }
-
-        // Step 2: if no breakpoint
-        if(ind == -1) {
-            reverse(arr, 0, n-1);
+        if(ind==-1)
+        {
+            reverse(nums,0,n-1);
             return;
         }
-
-        // Step 3: find next greater
-        for(int i = n-1; i > ind; i--) {
-            if(arr[i] > arr[ind]) {
-                swap(arr, i, ind);
+        for(int i=n-1;i>ind;i--)
+        {
+            if(nums[i]>nums[ind])
+            {
+                swap(nums,i,ind);
                 break;
             }
         }
+        reverse(nums,ind+1,n-1);
 
-        // Step 4: reverse right part
-        reverse(arr, ind+1, n-1);
+        
     }
-
-    public void swap(int[] arr, int i, int j) {
-        int t = arr[i];
-        arr[i] = arr[j];
-        arr[j] = t;
+    public void swap(int nums[], int i, int ind)
+    {
+        int t=nums[i];
+        nums[i]=nums[ind];
+        nums[ind]=t;
     }
-
-    public void reverse(int[] arr, int l, int r) {
-        while(l < r) {
-            swap(arr, l, r);
+    public void reverse(int nums[],int l,int r)
+    {
+        while(l<r)
+        {
+            swap(nums,l,r);
             l++;
             r--;
         }
