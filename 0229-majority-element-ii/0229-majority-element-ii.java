14805573a6e10a4b77fb293f8322fe0e7452e423
Aakanksha <138550872+AakankshaGupta16@users.noreply.github.com>
2026-07-06 01:12:45 +0530

    Time: 14 ms (41.45%), Space: 53.3 MB (10.33%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -3,37 +3,16 @@ class Solution {
     {
         int n=nums.length;
         List<Integer> ans=new ArrayList<>();
-        int cand1=0,cand2=0,c1=0,c2=0;
-        for(int num:nums)
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<n;i++)
         {
-            if(num==cand1) c1++;
-            else if (num==cand2) c2++;
-            else if (c1==0) 
-            {
-                cand1=num;
-                c1=1;
-            }
-            else if(c2==0)
-            {
-                cand2=num;
-                c2=1;
-            }
-            else
-            {
-                c1--;
-                c2--;
-            }
-       }
-       int count1=0,count2=0;
-       for(int num:nums)
-       {
-        if(num==cand1) count1++;
-        if(num==cand2 ) count2++;
-       }
-        if(count1>n/3) ans.add(cand1);
-        if(count2>n/3 && cand2 != cand1) ans.add(cand2);
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+        for(int key : map.keySet())
+        {
+            if(map.get(key) > n/3)
+            ans.add(key);
+        }
         return ans;
     }
 }
-
-
