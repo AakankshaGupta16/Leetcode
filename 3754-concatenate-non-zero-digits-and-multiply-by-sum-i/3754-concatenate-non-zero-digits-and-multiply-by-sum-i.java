402b113d2a3d9b2a7c0f024cf65f73a3e28f252d
Aakanksha <138550872+AakankshaGupta16@users.noreply.github.com>
2026-07-07 13:55:59 +0530

    Time: 1 ms (99.85%), Space: 42.8 MB (34.98%) - LeetHub

---
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.java
@@ -0,0 +1,28 @@
+class Solution 
+{
+    public long sumAndMultiply(int n) 
+    {
+        long rev = 0;
+
+        // Reverse the number
+        for (int i = n; i > 0; i /= 10) {
+            rev = rev * 10 + i % 10;
+        }
+
+        long sum = 0;
+        long no = 0;
+
+        // Process digits in original order
+        for (long i = rev; i > 0; i /= 10) 
+        {
+            int d = (int)(i % 10);
+            if (d != 0) 
+            {
+                no = no * 10 + d;
+                sum += d;
+            }
+        }
+
+        return no * sum;
+    }
+}
