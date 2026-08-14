b46be54e7a68f72856da07b8cf2577b65005ee07
Aakanksha <138550872+AakankshaGupta16@users.noreply.github.com>
2026-08-14 19:18:58 +0530

    Time: 2 ms (89.67%), Space: 44.3 MB (40.1%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,50 +1,37 @@
-class Pair
-{
-    int r,c,t;
-    Pair(int row, int col,int time)
-    {
-        this.r=row;
-        this.c=col;
-        this.t=time;
-    }
-}
-class Solution 
-{
+class Solution {
     public int orangesRotting(int[][] grid) 
     {
+        Queue<Pair>q=new LinkedList<>();
         int n=grid.length;
         int m=grid[0].length;
-        int freshcnt=0;
-        Queue<Pair> q=new LinkedList<>();
         int vis[][]=new int[n][m];
+        int cntfresh=0;
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
                 if(grid[i][j]==2)
                 {
-                    vis[i][j]=2;
-                    q.add(new Pair(i,j,0));
+                    q.offer(new Pair(i,j,0));
                 }
-                else if(grid[i][j]==0)
+                else
                 {
                     vis[i][j]=0;
                 }
-                else if(grid[i][j]==1)
-                freshcnt++;
+                if(grid[i][j]==1) cntfresh++;
             }
         }
         int tm=0;
+        int drow[]={-1,0,1,0};
+        int dcol[]={0,1,0,-1};
         int cnt=0;
-        int drow[]={-1,0,+1,0};
-        int dcol[]={0,+1,0,-1};
         while(!q.isEmpty())
         {
-            Pair p=q.poll();
-            int r=p.r;
-            int c=p.c;
-            int t=p.t;
-            tm=Math.max(t,tm);
+            int r=q.peek().row;
+            int c=q.peek().col;
+            int t=q.peek().tm;
+            tm=Math.max(tm,t);
+            q.poll();
             for(int i=0;i<4;i++)
             {
                 int nrow=r+drow[i];
@@ -52,14 +39,24 @@ class Solution
                 if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]==0 && grid[nrow][ncol]==1)
                 {
                     q.add(new Pair(nrow,ncol,t+1));
-                    vis[nrow][ncol]=1;
-                    cnt++;
-                }
-            }
+                    vis[nrow][ncol]=2;
+                    cntfresh--;
+                } 
+            }           
         }
-        if(cnt!=freshcnt) return -1;
+        if(cntfresh==0)
         return tm;
-
-        
+        else return -1;
     }
+}
+class Pair
+{
+    int row,col, tm;
+    Pair(int row, int col, int tm)
+    {
+        this.row=row;
+        this.col=col;
+        this.tm=tm;
+    }
+
 }
