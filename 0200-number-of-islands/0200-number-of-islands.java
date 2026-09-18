cf5e1389a4d067359ca93c65fd88bc2acaf0fbc8
Aakanksha <138550872+AakankshaGupta16@users.noreply.github.com>
2026-09-18 13:18:45 +0530

    Time: 6 ms (21.24%), Space: 55 MB (5.07%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,6 +1,6 @@
 class Pair
 {
-    int first, second;
+    int first,second;
     public Pair(int first,int second)
     {
         this.first=first;
@@ -8,63 +8,50 @@ class Pair
     }
 }
 
-class Solution 
-{
+class Solution {
     public int numIslands(char[][] grid) 
     {
-        int c = grid[0].length;
-        int r = grid.length;
-        int vis[][] = new int[r][c];
-        int cnt = 0;
-
-        for(int i=0;i<r;i++)
+        int cnt=0;
+        int n=grid.length;
+        int m=grid[0].length;
+        int vis[][]=new int[n][m];
+        for(int i=0;i<n;i++)
         {
-            for(int j=0;j<c;j++)
+            for(int j=0;j<m;j++)
             {
-                // add visited check
-                if(grid[i][j] == '1' && vis[i][j] == 0)
+                if(vis[i][j]==0 && grid[i][j]=='1')
                 {
-                    bfs(i,j,grid,vis);
+                    bfs(i,j,n,m,vis,grid);
                     cnt++;
                 }
             }
         }
         return cnt;
     }
-
-    private void bfs(int ro,int co,char grid[][],int vis[][] )
+    public void bfs(int r, int c,int n, int m, int vis[][], char grid[][])
     {
-        vis[ro][co]=1;
+        vis[r][c]=1;
         Queue<Pair> q=new LinkedList<>();
-
-        // push starting node
-        q.add(new Pair(ro, co));
-
-        int n=grid.length;
-        int m=grid[0].length;
-
+        q.add(new Pair(r,c));
         while(!q.isEmpty())
         {
             int row=q.peek().first;
             int col=q.peek().second;
             q.remove();
-
-            // restrict to 4 directions only
-            int delRow[] = {-1, 0, 1, 0};
-            int delCol[] = {0, 1, 0, -1};
-
+            int delrow[]={-1,0,1,0};
+            int delcol[]={0,1,0,-1};
             for(int i=0;i<4;i++)
             {
-                int nrow=row+delRow[i];
-                int ncol=col+delCol[i];
-
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m 
-                   && grid[nrow][ncol]=='1' && vis[nrow][ncol]==0)
+                int nrow=row+delrow[i];
+                int ncol=col+delcol[i];
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]==0 && grid[nrow][ncol]=='1')
                 {
                     vis[nrow][ncol]=1;
                     q.add(new Pair(nrow,ncol));
                 }
             }
+
         }
+
     }
 }
